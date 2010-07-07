8e6735b1738324aba372bfdf2cbf0bcf93dd5882
Ben Smith <ben@metabroadcast.com>
2010-07-07 14:58:07 +0000

    added Publisher enum
    git-svn-id: https://uriplay.googlecode.com/svn/uriplay-media-reference/trunk@1218 9f0b3f36-cced-11dd-a405-31afbefa9e0e

---
+++ src/main/java/org/uriplay/media/entity/Publisher.java
@@ -0,0 +1,40 @@
+package org.uriplay.media.entity;
+
+import com.metabroadcast.common.base.Maybe;
+
+public enum Publisher {
+    BBC("BBC", "bbc.co.uk", Countries.GB),
+    C4("Channel 4", "channel4.com", Countries.GB),
+    HULU("Hulu", "hulu.com", Countries.US),
+    YOUTUBE("YouTube", "youtube.com", Countries.ALL),
+    TED("TED", "ted.com", Countries.ALL),
+    VIMEO("VIMEO", "vimeo.com", Countries.ALL);
+    
+    private final String key;
+    private final Country country;
+    private final String title;
+
+    Publisher(String title, String key, Country country) {
+        this.title = title;
+        this.key = key;
+        this.country = country;
+    }
+    public String title() {
+        return title;
+    }
+    public String key() {
+        return key;
+    }
+    public Country country() {
+        return country;
+    }
+    
+    public static Maybe<Publisher> fromKey(String key) {
+        for (Publisher publisher: Publisher.values()) {
+            if (key.equals(publisher.key())) {
+                return Maybe.just(publisher);
+            }
+        }
+        return Maybe.nothing();
+    }
+}
