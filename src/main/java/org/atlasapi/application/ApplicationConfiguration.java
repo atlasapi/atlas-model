7fe14888ecc6908cc8d9061d38901f13b67cd810
Fred van den Driessche <fred@metabroadcast.com>
2010-10-08 18:05:17 +0100

    removed references to exluded publishers

--- src/main/java/org/atlasapi/application/ApplicationConfiguration.java
+++ src/main/java/org/atlasapi/application/ApplicationConfiguration.java
@@ -4,43 +4,41 @@ import java.util.Set;
 
 import org.atlasapi.media.entity.Publisher;
 
-import com.google.common.collect.ImmutableList;
+import com.google.common.base.Predicate;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Sets;
 
 public class ApplicationConfiguration {
 	
 	public static final ApplicationConfiguration DEFAULT_CONFIGURATION = defaultConfiguration();
 	
-	private static ApplicationConfiguration defaultConfiguration() {
+	public static ApplicationConfiguration defaultConfiguration() {
 		ApplicationConfiguration defaultApp = new ApplicationConfiguration();
 		
-		defaultApp.getIncludedPublishers().addAll(ImmutableList.of(Publisher.BBC));
-		defaultApp.getExcludedPublishers().addAll(ImmutableList.of(Publisher.C4));
+		defaultApp.setIncludedPublishers(Iterables.filter(ImmutableSet.copyOf(Publisher.values()), new Predicate<Publisher>() {
+
+			@Override
+			public boolean apply(Publisher input) {
+				return !input.equals(Publisher.C4);
+			}
+		}));
 		
 		return defaultApp;
 	}
 
 	private Set<Publisher> includedPublishers = Sets.newHashSet();
-	private Set<Publisher> excludedPublishers = Sets.newHashSet();
 	
 	public ApplicationConfiguration() {
 		
 	}
 
-	public void setIncludedPublishers(Set<Publisher> includedPublishers) {
+	public void setIncludedPublishers(Iterable<Publisher> includedPublishers) {
 		this.includedPublishers = Sets.newHashSet(includedPublishers);
 	}
 
 	public Set<Publisher> getIncludedPublishers() {
 		return includedPublishers;
-	}
-	
-	public void setExcludedPublishers(Set<Publisher> excludedPublishers) {
-		this.excludedPublishers = Sets.newHashSet(excludedPublishers);
-	}
-
-	public Set<Publisher> getExcludedPublishers() {
-		return excludedPublishers;
 	}
 
 }
