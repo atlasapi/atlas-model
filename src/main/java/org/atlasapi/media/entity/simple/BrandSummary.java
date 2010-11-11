bfa7fddf243a5058343904d46ed3c2d9fb61b340
Ben Smith <ben@thesmith.co.uk>
2010-11-11 13:29:30 +0000

    added id to brand summary

--- src/main/java/org/atlasapi/media/entity/simple/BrandSummary.java
+++ src/main/java/org/atlasapi/media/entity/simple/BrandSummary.java
@@ -11,7 +11,6 @@ public class BrandSummary extends Identified {
 
 	private String title;
 	private String description;
-	private String id;
 	
 	public BrandSummary() { /* required for XML/JSON tools */ }
 	
@@ -34,12 +33,4 @@ public class BrandSummary extends Identified {
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	
-	public String getId() {
-        return id;
-    }
-	
-	public void setId(String id) {
-        this.id = id;
-    }
 }
