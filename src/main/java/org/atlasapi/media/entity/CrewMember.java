816a2e595ae6036f53dd00040825d47f9fa95410
Ben Smith <ben@thesmith.co.uk>
2011-02-23 22:52:42 +0000

    added presenter

--- src/main/java/org/atlasapi/media/entity/CrewMember.java
+++ src/main/java/org/atlasapi/media/entity/CrewMember.java
@@ -17,6 +17,7 @@ public class CrewMember extends Identified {
         SERIES_EDITOR("series_editor", "Series Editor"),
         SERIES_PRODUCER("series_producer", "Series Producer"),
         WRITER("writer", "Writer"),
+        PRESENTER("presenter", "Presenter"),
         ACTOR("actor", "Actor");
         
         private final String key;
