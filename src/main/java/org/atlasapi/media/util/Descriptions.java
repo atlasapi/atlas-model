7f81c0cffde8a7ef7ed11d3cbb701c15e60e1483
Daniel Bell <shinyhappydan@gmail.com>
2011-05-17 17:28:26 +0100

    Add Person helper methods Take ? extends Identified rather than Description in arguments

--- src/main/java/org/atlasapi/media/util/Descriptions.java
+++ src/main/java/org/atlasapi/media/util/Descriptions.java
@@ -3,7 +3,9 @@ package org.atlasapi.media.util;
 import java.util.List;
 
 import org.atlasapi.media.entity.simple.Description;
+import org.atlasapi.media.entity.simple.Identified;
 import org.atlasapi.media.entity.simple.Item;
+import org.atlasapi.media.entity.simple.Person;
 import org.atlasapi.media.entity.simple.Playlist;
 
 import com.google.common.base.Function;
@@ -16,38 +18,56 @@ public class Descriptions {
     private Descriptions() {
     }
     
-    public static List<Item> getItems(Iterable<? extends Description> descriptions) {
+    public static List<Item> getItems(Iterable<? extends Identified> descriptions) {
         return ImmutableList.copyOf(Iterables.transform(Iterables.filter(descriptions, IS_ITEM), TO_ITEM));
     }
     
-    public static List<Playlist> getPlaylists(Iterable<? extends Description> descriptions) {
+    public static List<Playlist> getPlaylists(Iterable<? extends Identified> descriptions) {
         return ImmutableList.copyOf(Iterables.transform(Iterables.filter(descriptions, IS_PLAYLIST), TO_PLAYLIST));
     }
     
-    public static final Predicate<Description> IS_PLAYLIST = new Predicate<Description>() {
+    public static List<Person> getPeople(Iterable<? extends Identified> descriptions) {
+        return ImmutableList.copyOf(Iterables.transform(Iterables.filter(descriptions, IS_PERSON), TO_PERSON));
+    }
+    
+    public static final Predicate<Identified> IS_PERSON = new Predicate<Identified>() {
         @Override
-        public boolean apply(Description input) {
+        public boolean apply(Identified input) {
+            return input instanceof Person;
+        }
+    };
+    
+    public static final Predicate<Identified> IS_PLAYLIST = new Predicate<Identified>() {
+        @Override
+        public boolean apply(Identified input) {
             return input instanceof Playlist;
         }
     };
     
-    public static final Predicate<Description> IS_ITEM = new Predicate<Description>() {
+    public static final Predicate<Identified> IS_ITEM = new Predicate<Identified>() {
         @Override
-        public boolean apply(Description input) {
+        public boolean apply(Identified input) {
             return input instanceof Item;
         }
     };
     
-    public static final Function<Description, Playlist> TO_PLAYLIST = new Function<Description, Playlist>() {
+    public static final Function<Identified, Person> TO_PERSON = new Function<Identified, Person>() {
         @Override
-        public Playlist apply(Description input) {
+        public Person apply(Identified input) {
+            return (Person) input;
+        }
+    };
+    
+    public static final Function<Identified, Playlist> TO_PLAYLIST = new Function<Identified, Playlist>() {
+        @Override
+        public Playlist apply(Identified input) {
             return (Playlist) input;
         }
     };
     
-    public static final Function<Description, Item> TO_ITEM = new Function<Description, Item>() {
+    public static final Function<Identified, Item> TO_ITEM = new Function<Identified, Item>() {
         @Override
-        public Item apply(Description input) {
+        public Item apply(Identified input) {
             return (Item) input;
         }
     };
