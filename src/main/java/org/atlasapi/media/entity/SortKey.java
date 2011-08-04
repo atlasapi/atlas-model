cb20b6cf54c13d18c701b54e39968089492c1d6b
Fred van den Driessche <fred@metabroadcast.com>
2011-08-04 17:41:04 +0100

    reverse ordering of serial and broadcast sort keys

--- src/main/java/org/atlasapi/media/entity/SortKey.java
+++ src/main/java/org/atlasapi/media/entity/SortKey.java
@@ -1,5 +1,7 @@
 package org.atlasapi.media.entity;
 
+import java.util.Comparator;
+
 import org.joda.time.DateTime;
 
 public enum SortKey {
@@ -73,6 +75,32 @@ public enum SortKey {
             }
         }
         return "99";
+    }
+    
+    
+    public static class SortKeyOutputComparator implements Comparator<String> {
+
+        @Override
+        public int compare(String sk1, String sk2) {
+            String prefix1 = sk1.substring(0, 2);
+            String prefix2 = sk2.substring(0, 2);
+            
+            int compareType = prefix1.compareTo(prefix2);
+            
+            if(compareType != 0) {
+                return compareType;
+            }
+            
+            String key1 = sk1.substring(2);
+            String key2 = sk2.substring(2);
+            
+            if(prefix1.equals("20") || prefix1.equals("30")) {
+                return key2.compareTo(key1);
+            }
+            
+            return key1.compareTo(key2);
+        }
+        
     }
     
 }
--- src/test/java/org/atlasapi/media/entity/SortKeyTest.java
+++ src/test/java/org/atlasapi/media/entity/SortKeyTest.java
@@ -1,6 +1,6 @@
 package org.atlasapi.media.entity;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.List;
 
@@ -40,18 +40,45 @@ public class SortKeyTest {
         
         Item deflt = new Item("default", "defaultCurie", Publisher.BBC);
         
-        List<Item> randomItems = ImmutableList.of(episode, deflt, broadcast, adapter);
+        List<String> randomKeys = Lists.transform(ImmutableList.of(episode, deflt, broadcast, adapter), TO_SORT_KEY);
         
-        List<String> randomKeys = Lists.transform(randomItems, TO_SORT_KEY);
-        
-        List<Item> orderedItems = ImmutableList.of(adapter, episode, broadcast, deflt);
-        
-        List<String> orderedKeys = Lists.transform(orderedItems, TO_SORT_KEY);
+        List<String> orderedKeys = Lists.transform(ImmutableList.of(adapter, episode, broadcast, deflt), TO_SORT_KEY);
         
         assertEquals(orderedKeys, Ordering.natural().immutableSortedCopy(randomKeys));
         
     }
 
-    
-    
+    @Test
+    public void testKeyOrdering() {
+        
+        Item adapter = new Item("adapter","adapterCurie", Publisher.BBC).withSortKey("asdf");
+        
+        Episode episode = new Episode("episode", "episodeCurie", Publisher.BBC);
+        episode.setSeriesNumber(4);
+        episode.setEpisodeNumber(5);
+        
+        Episode episode1 = new Episode("episode1", "episodeCurie", Publisher.BBC);
+        episode1.setSeriesNumber(1);
+        episode1.setEpisodeNumber(6);
+        
+        Item broadcast = new Item("broadcast", "broadcastCurie", Publisher.BBC);
+        Version version = new Version();
+        version.addBroadcast(new Broadcast("channel9", new DateTime(2011, 10, 10, 0, 0,0,0, DateTimeZone.UTC), new DateTime(2011, 10, 10, 1, 0,0,0, DateTimeZone.UTC)));
+        broadcast.addVersion(version);
+
+        Item broadcast1 = new Item("broadcast", "broadcastCurie", Publisher.BBC);
+        Version version1 = new Version();
+        version1.addBroadcast(new Broadcast("channel10", new DateTime(2011, 10, 9, 0, 0,0,0, DateTimeZone.UTC), new DateTime(2011, 10, 9, 1, 0,0,0, DateTimeZone.UTC)));
+        broadcast1.addVersion(version1);
+        
+        Item deflt = new Item("default", "defaultCurie", Publisher.BBC);
+        
+        List<String> randomKeys = Lists.transform(ImmutableList.of(episode, broadcast1, deflt, episode1, broadcast, adapter), TO_SORT_KEY);
+        
+        List<String> orderedKeys = Lists.transform(ImmutableList.of(adapter, episode, episode1, broadcast, broadcast1, deflt), TO_SORT_KEY);
+        
+        assertEquals(orderedKeys, Ordering.from(new SortKey.SortKeyOutputComparator()).immutableSortedCopy(randomKeys));
+        
+    }
+
 }
