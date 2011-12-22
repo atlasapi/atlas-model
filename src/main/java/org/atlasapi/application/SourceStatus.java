a70f07500d7fd55d011f19da6431fef10cdeca7b
Fred van den Driessche <fred@metabroadcast.com>
2011-12-22 18:42:05 +0000

    introduced source status to manage sources in applications

--- src/main/java/org/atlasapi/application/SourceStatus.java
+++ src/main/java/org/atlasapi/application/SourceStatus.java
@@ -1,13 +1,22 @@
 package org.atlasapi.application;
 
 import com.google.common.base.Preconditions;
+import com.google.common.base.Predicate;
 
 public class SourceStatus {
 
     public static final SourceStatus UNAVAILABLE = new SourceStatus(SourceState.UNAVAILABLE, false);
     public static final SourceStatus REVOKED = new SourceStatus(SourceState.REVOKED, false);
+    public static final SourceStatus REQUESTED = new SourceStatus(SourceState.REQUESTED, false);
     public static final SourceStatus AVAILABLE_ENABLED = new SourceStatus(SourceState.AVAILABLE, true);
     public static final SourceStatus AVAILABLE_DISABLED = new SourceStatus(SourceState.AVAILABLE, false);
+    
+    public static final Predicate<SourceStatus> IS_ENABLED = new Predicate<SourceStatus>(){
+
+        @Override
+        public boolean apply(SourceStatus input) {
+            return input.isEnabled();
+        }};
     
     public enum SourceState {
         
@@ -22,20 +31,63 @@ public class SourceStatus {
     private final boolean enabled;
 
     private SourceStatus(SourceState state, boolean enabled) {
+        Preconditions.checkArgument(!enabled || (state == SourceState.AVAILABLE && enabled));
         this.state = state;
         this.enabled = enabled;
     }
     
     public SourceStatus copyWithState(SourceState state) {
-        return new SourceStatus(state, enabled);
+        return new SourceStatus(state, isEnabled());
     }
     
     public SourceStatus enable() {
         Preconditions.checkState(state == SourceState.AVAILABLE);
-        return new SourceStatus(state, true);
+        return AVAILABLE_ENABLED;
     }
     
     public SourceStatus disable() {
-        return new SourceStatus(state, false);
+        Preconditions.checkState(state == SourceState.AVAILABLE);
+        return AVAILABLE_DISABLED;
+    }
+
+    public SourceStatus request() {
+        Preconditions.checkState(state == SourceState.UNAVAILABLE);
+        return REQUESTED;
+    }
+    
+    public SourceStatus approve() {
+        Preconditions.checkState(state == SourceState.REQUESTED);
+        return AVAILABLE_DISABLED;
+    }
+    
+    public SourceStatus revoke() {
+        Preconditions.checkState(state == SourceState.AVAILABLE);
+        return REVOKED;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public SourceState getState() {
+        return state;
+    }
+    
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that instanceof SourceStatus) {
+            SourceStatus other = (SourceStatus) that;
+            return enabled == other.enabled && state == other.state;
+        }
+        return false;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return String.format("%s (%s)", state.toString().toLowerCase(), enabled ? "enabled" : "disabled");
     }
 }
