2b13c1df015bc594adfa0ed43dfcad952e31a4c7
Tom McAdam <tom@metabroadcast.com>
2012-02-01 09:08:11 +0000

    Fix merge problem

--- src/main/java/org/atlasapi/media/entity/Publisher.java
+++ src/main/java/org/atlasapi/media/entity/Publisher.java
@@ -37,7 +37,7 @@ public enum Publisher {
     PREVIEW_NETWORKS("Preview Networks", "previewnetworks.com", Countries.GB, SourceStatus.UNAVAILABLE),
     ARCHIVE_ORG("Archive.org", "archive.org", Countries.ALL, SourceStatus.AVAILABLE_ENABLED),
 	WORLD_SERVICE("BBC World Service Archive", "wsarchive.bbc.co.uk", Countries.ALL, SourceStatus.UNAVAILABLE),
-	BBC_REDUX("BBC Redux", "bbcredux.com", Countries.GB, SourceStatus.UNAVAILABLE);
+	BBC_REDUX("BBC Redux", "bbcredux.com", Countries.GB, SourceStatus.UNAVAILABLE),
     METABROADCAST("MetaBroadcast", "metabroadcast.com", Countries.ALL, SourceStatus.AVAILABLE_ENABLED),
 	DBPEDIA("DBpedia", "dbpedia.org", Countries.ALL, SourceStatus.AVAILABLE_ENABLED);
 	
