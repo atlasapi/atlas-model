6cd0ac3bfe77ab02690f6f1722f785b8de92eb4b
Daniel Bell <shinyhappydan@gmail.com>
2012-02-20 20:03:12 +0000

    Move Annotation to this package

---
+++ src/main/java/org/atlasapi/output/Annotation.java
@@ -0,0 +1,59 @@
+package org.atlasapi.output;
+
+import java.util.Set;
+
+import com.google.common.base.Function;
+import com.google.common.base.Functions;
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Maps;
+import com.metabroadcast.common.text.MoreStrings;
+
+public enum Annotation {
+
+    DESCRIPTION,
+    EXTENDED_DESCRIPTION,
+    BRAND_SUMMARY,   
+    SERIES_SUMMARY,
+    SUB_ITEMS,
+    CLIPS,
+    PEOPLE,
+    TOPICS,
+    SEGMENT_EVENTS,
+    RELATED_LINKS,
+    KEY_PHRASES,
+    BROADCASTS,   
+    LOCATIONS,
+    FIRST_BROADCASTS,
+    NEXT_BROADCASTS,
+    AVAILABLE_LOCATIONS,
+    UPCOMING;
+    
+    private static final ImmutableSet<Annotation> defaultAnnotations = ImmutableSet.of(
+        DESCRIPTION,
+        EXTENDED_DESCRIPTION,
+        SUB_ITEMS,
+        BROADCASTS,
+        LOCATIONS,
+        PEOPLE,
+        CLIPS
+    );
+    public static final BiMap<String, Annotation> LOOKUP = HashBiMap.create(Maps.uniqueIndex(ImmutableList.copyOf(Annotation.values()), Functions.compose(MoreStrings.TO_LOWER, Functions.toStringFunction())));
+    
+    public static final Set<Annotation> defaultAnnotations() {
+        return defaultAnnotations;
+    }
+    
+    public String toKey() {
+        return name().toLowerCase();
+    }
+    
+    public static final Function<Annotation, String> TO_KEY = new Function<Annotation, String>() {
+        @Override
+        public String apply(Annotation input) {
+            return input.toKey();
+        }
+    };
+}
