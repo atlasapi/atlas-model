4bf0620da43a24a4cdccc2e5a80f47be0a97dca2
Sergio Bossa <sergio.bossa@gmail.com>
2012-05-15 18:05:37 +0100

    Fixed Country json configuration.

--- src/main/java/org/atlasapi/serialization/json/CountryConfiguration.java
+++ src/main/java/org/atlasapi/serialization/json/CountryConfiguration.java
@@ -2,12 +2,12 @@ package org.atlasapi.serialization.json;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.metabroadcast.common.intl.Country;
 
 /**
  */
-public interface CountryConfiguration {
-    
+public abstract class CountryConfiguration {
+
     @JsonCreator
-    Country make(@JsonProperty("code") String code, @JsonProperty("name") String name);
+    CountryConfiguration(@JsonProperty("code") String code, @JsonProperty("name") String name) {
+    }
 }
