d5c14296992eadc589f5d5acbcb8910579626255
Sergio Bossa <sergio.bossa@gmail.com>
2012-05-15 16:35:36 +0100

    Improved JSON serialization.

--- src/main/java/org/atlasapi/serialization/json/ItemConfiguration.java
+++ src/main/java/org/atlasapi/serialization/json/ItemConfiguration.java
@@ -6,4 +6,8 @@ import com.fasterxml.jackson.annotation.JsonFilter;
  */
 @JsonFilter(value = "Item")
 public interface ItemConfiguration {
+
+    public static final String FILTER = "Item";
+    public static final String CLIPS_FILTER = "clips";
+    public static final String VERSIONS_FILTER = "versions";
 }
