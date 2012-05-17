5347b8772aa0fac86a4696602654b60772a786df
Sergio Bossa <sergio.bossa@gmail.com>
2012-05-17 11:46:47 +0100

    Updated model to allow for searching by specialization.

--- src/main/java/org/atlasapi/search/model/SearchQuery.java
+++ src/main/java/org/atlasapi/search/model/SearchQuery.java
@@ -5,23 +5,35 @@ import java.util.Set;
 import org.atlasapi.media.entity.Publisher;
 
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
 import com.metabroadcast.common.query.Selection;
+import org.atlasapi.media.entity.Specialization;
 
 public class SearchQuery {
 
 	private final String term;
 	private final Selection selection;
+    private final Set<Specialization> includedSpecializations;
 	private final Set<Publisher> includedPublishers;
     private final float titleWeighting;
     private final float broadcastWeighting;
     private final float catchupWeighting;
 
+    public SearchQuery(String term, Selection selection, float titleWeighting, float broadcastWeighting, float availabilityWeighting) {
+		this(term, selection, Sets.<Specialization>newHashSet(), Sets.<Publisher>newHashSet(), titleWeighting, broadcastWeighting, availabilityWeighting);
+	}
+    
 	public SearchQuery(String term, Selection selection, Iterable<Publisher> includedPublishers, float titleWeighting, float broadcastWeighting, float availabilityWeighting) {
+		this(term, selection, Sets.<Specialization>newHashSet(), includedPublishers, titleWeighting, broadcastWeighting, availabilityWeighting);
+	}
+    
+    public SearchQuery(String term, Selection selection, Iterable<Specialization> includedSpecializations, Iterable<Publisher> includedPublishers, float titleWeighting, float broadcastWeighting, float availabilityWeighting) {
 		this.term = term;
 		this.selection = selection;
         this.titleWeighting = titleWeighting;
         this.broadcastWeighting = broadcastWeighting;
         this.catchupWeighting = availabilityWeighting;
+        this.includedSpecializations = ImmutableSet.copyOf(includedSpecializations);
 		this.includedPublishers = ImmutableSet.copyOf(includedPublishers);
 	}
 	
@@ -32,6 +44,10 @@ public class SearchQuery {
 	public Selection getSelection() {
 		return selection;
 	}
+    
+    public Set<Specialization> getIncludedSpecializations() {
+        return includedSpecializations;
+    }
 	
 	public Set<Publisher> getIncludedPublishers() {
 		return includedPublishers;
