b213660685655d5a4aace58443e70f1fcc1ca27c
Sergio Bossa <sergio.bossa@gmail.com>
2012-05-18 10:51:42 +0100

    Fixed json deserialization bugs.

--- src/main/java/org/atlasapi/serialization/json/BroadcastConfiguration.java
+++ src/main/java/org/atlasapi/serialization/json/BroadcastConfiguration.java
@@ -2,13 +2,14 @@ package org.atlasapi.serialization.json;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.atlasapi.media.entity.Broadcast;
 import org.joda.time.DateTime;
 
 /**
  */
-public interface BroadcastConfiguration {
+public abstract class  BroadcastConfiguration {
     
     @JsonCreator
-    Broadcast make(@JsonProperty("broadcastOn") String broadcastOn, @JsonProperty("transmissionTime") DateTime transmissionTime, @JsonProperty("transmissionEndTime")  DateTime transmissionEndTime, @JsonProperty("activelyPublished")  Boolean activelyPublished);
+    BroadcastConfiguration(@JsonProperty("broadcastOn") String broadcastOn, @JsonProperty("transmissionTime") DateTime transmissionTime, @JsonProperty("transmissionEndTime")  DateTime transmissionEndTime, @JsonProperty("activelyPublished")  Boolean activelyPublished) {
+        
+    }
 }
