9897c7f5a772b7f696f91e2d843d09582076f08b
Sergio Bossa <sergio.bossa@gmail.com>
2012-06-15 12:46:02 +0100

    Improved json serialization by better organizing configuration modules and enabling default unfiltering behavior.

--- src/main/java/org/atlasapi/serialization/json/ObjectConfiguration.java
+++
@@ -1,9 +0,0 @@
-package org.atlasapi.serialization.json;
-
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-
-/**
- */
-@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
-public interface ObjectConfiguration {
-}
