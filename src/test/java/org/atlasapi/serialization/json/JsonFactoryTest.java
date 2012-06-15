b3742054fe34f86b61c67453e242066e6dfebed7
Sergio Bossa <sergio.bossa@gmail.com>
2012-06-15 12:46:02 +0100

    Improved json serialization by better organizing configuration modules and enabling default unfiltering behavior.

--- src/test/java/org/atlasapi/serialization/json/JsonFactoryTest.java
+++ src/test/java/org/atlasapi/serialization/json/JsonFactoryTest.java
@@ -8,12 +8,12 @@ import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.google.common.collect.Sets;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import org.atlasapi.media.entity.Clip;
 import org.atlasapi.media.entity.Content;
 import org.atlasapi.media.entity.Item;
 import org.atlasapi.media.entity.Publisher;
 import org.atlasapi.media.entity.Version;
+import org.atlasapi.serialization.json.configuration.model.FilteredItemConfiguration;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -22,44 +22,54 @@ import static org.junit.Assert.*;
 public class JsonFactoryTest {
 
     @Test
-    public void testItemCanBeReadBackAsGenericContent() throws Exception {
-        Item item = new Item("uri", "curie", Publisher.BBC);
-        item.setId(1L);
-        item.setClips(Arrays.asList(new Clip("uri", "curie", Publisher.BBC)));
-        assertEquals(1, item.getClips().size());
-
-        ObjectMapper mapper = JsonFactory.makeJsonMapper();
-        FilterProvider filters = new SimpleFilterProvider().addFilter("Item", SimpleBeanPropertyFilter.serializeAllExcept(Collections.EMPTY_SET));
-
-        String json = mapper.writer(filters).writeValueAsString(item);
-        Content read = mapper.readValue(json, Content.class);
-        assertEquals(1, read.getClips().size());
-    }
-
-    @Test
-    public void testItemWithFiltering() throws Exception {
+    public void testUnfilteredItem() throws Exception {
         Item item = new Item("uri", "curie", Publisher.BBC);
         item.setId(1L);
         item.setClips(Arrays.asList(new Clip("uri", "curie", Publisher.BBC)));
         item.setVersions(Sets.newHashSet(new Version()));
-        assertEquals(1, item.getClips().size());
-        assertEquals(1, item.getVersions().size());
-
 
         ObjectMapper mapper = JsonFactory.makeJsonMapper();
-        FilterProvider filters = new SimpleFilterProvider().addFilter("Item", SimpleBeanPropertyFilter.serializeAllExcept("clips", "versions"));
 
-        String itemJson = mapper.writer(filters).writeValueAsString(item);
+        String json = mapper.writeValueAsString(item);
+        Item read = mapper.readValue(json, Item.class);
+        assertEquals(1, read.getClips().size());
+        assertEquals(1, read.getVersions().size());
+    }
+
+    @Test
+    public void testFilteredItem() throws Exception {
+        Item item = new Item("uri", "curie", Publisher.BBC);
+        item.setId(1L);
+        item.setClips(Arrays.asList(new Clip("uri", "curie", Publisher.BBC)));
+        item.setVersions(Sets.newHashSet(new Version()));
+
+        ObjectMapper mapper = JsonFactory.makeJsonMapper();
+        FilterProvider filters = new SimpleFilterProvider().addFilter(FilteredItemConfiguration.FILTER, SimpleBeanPropertyFilter.serializeAllExcept(FilteredItemConfiguration.CLIPS_FILTER, FilteredItemConfiguration.VERSIONS_FILTER));
+        mapper.setFilters(filters);
+
+        String itemJson = mapper.writeValueAsString(item);
         Item readItem = mapper.readValue(itemJson, Item.class);
         assertEquals(0, readItem.getClips().size());
         assertEquals(0, readItem.getVersions().size());
 
-        String clipsJson = mapper.writer(filters).writeValueAsString(item.getClips());
+        String clipsJson = mapper.writeValueAsString(item.getClips());
         Collection<Clip> readClips = mapper.readValue(clipsJson, TypeFactory.defaultInstance().constructCollectionType(Collection.class, Clip.class));
         assertEquals(1, readClips.size());
-        
-        String versionsJson = mapper.writer(filters).writeValueAsString(item.getVersions());
+
+        String versionsJson = mapper.writeValueAsString(item.getVersions());
         Collection<Version> readVersions = mapper.readValue(versionsJson, TypeFactory.defaultInstance().constructCollectionType(Collection.class, Version.class));
         assertEquals(1, readVersions.size());
+    }
+
+    @Test
+    public void testItemCanBeReadBackAsGenericContent() throws Exception {
+        Item item = new Item("uri", "curie", Publisher.BBC);
+        item.setId(1L);
+
+        ObjectMapper mapper = JsonFactory.makeJsonMapper();
+
+        String json = mapper.writeValueAsString(item);
+        Content read = mapper.readValue(json, Content.class);
+        assertTrue(read instanceof Item);
     }
 }
