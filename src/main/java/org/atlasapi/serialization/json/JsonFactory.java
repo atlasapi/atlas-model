f516d349b2882c015bac3db6abbb44a80efce35c
Sergio Bossa <sergio.bossa@gmail.com>
2012-06-18 19:44:08 +0100

    Fixed json serialization to be compatible with existent data.

--- src/main/java/org/atlasapi/serialization/json/JsonFactory.java
+++ src/main/java/org/atlasapi/serialization/json/JsonFactory.java
@@ -58,7 +58,7 @@ public class JsonFactory {
         }
     }
     
-    @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.WRAPPER_ARRAY, property = "@class")
+    @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
     private static interface ObjectConfiguration {
     }
 }
