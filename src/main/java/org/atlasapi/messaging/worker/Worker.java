c588b51f7b85fdd3ac7dfb0725b3382ed5a5978d
Sergio Bossa <sergio.bossa@gmail.com>
2012-06-21 11:34:39 +0100

    Moved org.atlasapi.persistence.messaging.* stuff into org.atlasapi.messaging.* (bad previous refactoring probably).

--- src/main/java/org/atlasapi/messaging/worker/Worker.java
+++ src/main/java/org/atlasapi/messaging/worker/Worker.java
@@ -1,6 +1,6 @@
-package org.atlasapi.persistence.messaging.worker;
+package org.atlasapi.messaging.worker;
 
-import org.atlasapi.persistence.messaging.event.EntityUpdatedEvent;
+import org.atlasapi.messaging.event.EntityUpdatedEvent;
 
 /**
  * Base interface for workers that need to process {@link org.atlasapi.persistence.messaging.event.Event}s.
