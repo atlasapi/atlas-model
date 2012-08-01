f49c9c0da3c88b5c2ef905e3318891202c1526e6
Sergio Bossa <sergio.bossa@gmail.com>
2012-08-01 15:37:18 +0100

    Renamed Event to Message.

--- src/main/java/org/atlasapi/messaging/worker/Worker.java
+++ src/main/java/org/atlasapi/messaging/worker/Worker.java
@@ -1,14 +1,14 @@
 package org.atlasapi.messaging.worker;
 
-import org.atlasapi.messaging.event.EntityUpdatedEvent;
+import org.atlasapi.messaging.EntityUpdatedMessage;
 
 /**
- * Base interface for workers that need to process {@link org.atlasapi.persistence.messaging.event.Event}s.
+ * Base interface for workers that need to process {@link org.atlasapi.persistence.messaging.Message}s.
  */
 public interface Worker {
     
     /**
-     * Process a {@link org.atlasapi.persistence.messaging.event.EntityUpdatedEvent}.
+     * Process a {@link org.atlasapi.persistence.messaging.EntityUpdatedMessage}.
      */
-    void process(EntityUpdatedEvent command);
+    void process(EntityUpdatedMessage command);
 }
