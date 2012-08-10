1a104f09d22df86c4897034a0910647a2b2f9fdb
Fred van den Driessche <fred@metabroadcast.com>
2012-08-10 18:04:20 +0100

    Add very basic equivalent content implementation

--- src/main/java/org/atlasapi/persistence/content/EquivalentContent.java
+++ src/main/java/org/atlasapi/persistence/content/EquivalentContent.java
@@ -1,8 +1,41 @@
 package org.atlasapi.persistence.content;
 
+import java.util.Map;
+import java.util.Set;
 
-public class EquivalentContent {
-    
-    
+import org.atlasapi.media.entity.Content;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+
+
+public class EquivalentContent /* extends ForwardingMap? */ {
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+
+        private ImmutableMap.Builder<String, Set<Content>> entries = ImmutableMap.builder();
+
+        public void put(String uri, Set<Content> equivalentSet) {
+            this.entries.put(uri, ImmutableSet.copyOf(equivalentSet));
+        }
+
+        public EquivalentContent build() {
+            return new EquivalentContent(entries.build());
+        }
+        
+    }
+
+    private Map<String,Set<Content>> entries;
+    
+    public EquivalentContent(ImmutableMap<String, Set<Content>> entries) {
+        this.entries = entries;
+    }
+
+    public Set<Content> equivalentContentFor(String uri) {
+        return entries.get(uri);
+    }
 }
