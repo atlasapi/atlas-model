0a59f014412a009f08ac15103b7e868f0cba553f
Sergio Bossa <sergio.bossa@gmail.com>
2012-08-14 15:15:56 +0100

    Implemented replay messages.

--- src/main/java/org/atlasapi/messaging/worker/Worker.java
+++ src/main/java/org/atlasapi/messaging/worker/Worker.java
@@ -1,6 +1,9 @@
 package org.atlasapi.messaging.worker;
 
+import org.atlasapi.messaging.BeginReplayMessage;
+import org.atlasapi.messaging.EndReplayMessage;
 import org.atlasapi.messaging.EntityUpdatedMessage;
+import org.atlasapi.messaging.ReplayMessage;
 
 /**
  * Base interface for workers that need to process {@link org.atlasapi.persistence.messaging.Message}s.
@@ -10,5 +13,20 @@ public interface Worker {
     /**
      * Process a {@link org.atlasapi.persistence.messaging.EntityUpdatedMessage}.
      */
-    void process(EntityUpdatedMessage command);
+    void process(EntityUpdatedMessage message);
+    
+    /**
+     * Process a {@link org.atlasapi.persistence.messaging.BeginReplayMessage}.
+     */
+    void process(BeginReplayMessage message);
+    
+    /**
+     * Process a {@link org.atlasapi.persistence.messaging.EndReplayMessage}.
+     */
+    void process(EndReplayMessage message);
+    
+    /**
+     * Process a {@link org.atlasapi.persistence.messaging.ReplayMessage}.
+     */
+    void process(ReplayMessage message);
 }
