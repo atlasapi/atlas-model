f3265a995df1c5e0ee6b8bc20f9eacf440dfdd05
Fred van den Driessche <fred@metabroadcast.com>
2012-12-19 16:08:40 +0000

    add Annotation request names

--- src/main/java/org/atlasapi/output/Annotation.java
+++ src/main/java/org/atlasapi/output/Annotation.java
@@ -3,6 +3,8 @@ package org.atlasapi.output;
 import java.util.Map;
 import java.util.Set;
 
+import javax.annotation.Nullable;
+
 import com.google.common.base.Function;
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableSet;
@@ -11,43 +13,57 @@ import com.metabroadcast.common.collect.ImmutableOptionalMap;
 
 public enum Annotation {
     //The order of these entries defines the order of output fields
-    ID_SUMMARY,
-    ID,
-    EXTENDED_ID, 
-    DESCRIPTION,
-    EXTENDED_DESCRIPTION,
-    BRAND_REFERENCE,
-    BRAND_SUMMARY,
-    SERIES_REFERENCE,
-    SERIES_SUMMARY,
-    SUB_ITEMS,
-    CLIPS,
-    PEOPLE,
-    TOPICS,
-    CONTENT_GROUPS,
-    SEGMENT_EVENTS,
-    RELATED_LINKS,
-    KEY_PHRASES,
-    BROADCASTS,   
-    LOCATIONS,
-    FIRST_BROADCASTS,
-    NEXT_BROADCASTS,
-    AVAILABLE_LOCATIONS,
-    UPCOMING, 
-    FILTERING_RESOURCE,
-    CHANNEL,
-    PRODUCTS,
-    RECENTLY_BROADCAST,
-    CHANNELS,
-    CHANNEL_GROUPS,
-    HISTORY,
-    PARENT,
-    VARIATIONS,
-    CHANNEL_SUMMARY,
-    AUDIT,
-    SERIES,
-    PUBLISHER,
-    IMAGES;
+    LICENSE("content.license"),
+    ID_SUMMARY("*.id_summary"),
+    ID("*.id"),
+    EXTENDED_ID("*.extended_id"), 
+    DESCRIPTION("content.description"),
+    EXTENDED_DESCRIPTION("content.extended_description"),
+    BRAND_REFERENCE("content.container"),
+    BRAND_SUMMARY("content.container_summary"),
+    SERIES_REFERENCE("content.series"),
+    SERIES_SUMMARY("content.series_summary"),
+    SUB_ITEMS("content.sub_items"),
+    CLIPS("content.clips"),
+    PEOPLE("content.people"),
+    TOPICS("content.topics"),
+    CONTENT_GROUPS("content.content_groups"),
+    SEGMENT_EVENTS("content.segment_events"),
+    RELATED_LINKS("content.related_links"),
+    KEY_PHRASES("content.key_phrases"),
+    BROADCASTS("content.broadcasts"),   
+    LOCATIONS("content.locations"),
+    FIRST_BROADCASTS("content.first_broadcasts"),
+    NEXT_BROADCASTS("content.next_broadcasts"),
+    AVAILABLE_LOCATIONS("content.available_locations"),
+    UPCOMING("content.upcoming"), 
+    FILTERING_RESOURCE("content.filtering_resource"),
+    CHANNEL("channel.detail"),
+    CHANNEL_GROUPS("channel.groups"),
+    HISTORY("channel.history"),
+    PARENT("channel.parent"),
+    PRODUCTS("content.products"),
+    RECENTLY_BROADCAST("content.recently_broadcast"),
+    CHANNELS("content.channels"),
+
+    CONTENT_CHANNEL_SUMMARY("content.channel_summary"),
+    SERIES("content.series"),
+    PUBLISHER("content.publisher"),
+    CONTENT_SUMMARY("content.summary"),
+    CONTENT_DETAIL("content.detail"),
+    CHANNEL_SUMMARY("channel.summary"),
+    AUDIT("content.audit"),
+    IMAGES("content.images");
+    
+    private final String requestName;
+
+    private Annotation(String requestName) {
+        this.requestName = requestName;
+    }
+    
+    public String getRequestName() {
+        return requestName;
+    }
     
     public String toKey() {
         return name().toLowerCase();
@@ -60,14 +76,39 @@ public enum Annotation {
         }
     };
     
+    private static final Function<Annotation, String> TO_REQUEST_NAME = new Function<Annotation, String>() {
+        @Override
+        public String apply(@Nullable Annotation input) {
+            return input.getRequestName();
+        }
+    };
+    
     public static final Function<Annotation, String> toKeyFunction() {
         return TO_KEY;
     }
 
+    public static final Function<Annotation, String> toRequestName() {
+        return TO_REQUEST_NAME;
+    }
+    
     private static final ImmutableSet<Annotation> ALL = ImmutableSet.copyOf(values());
     
     public static final ImmutableSet<Annotation> all() {
         return ALL;
+    }
+
+    private static final Map<String,Optional<Annotation>> nameLookup = ImmutableOptionalMap.fromMap(Maps.uniqueIndex(all(), TO_REQUEST_NAME));
+    
+    public static final Map<String, Optional<Annotation>> nameLookup() {
+        return nameLookup;
+    }
+    
+    public static final Optional<Annotation> fromRequestName(String name, Optional<String> context) {
+        return nameLookup.get(requestNameForContext(name, context));
+    }
+
+    public static String requestNameForContext(String name, Optional<String> context) {
+        return context.isPresent() ? context.get() + "." + name : name;
     }
 
     private static final Map<String,Optional<Annotation>> lookup = ImmutableOptionalMap.fromMap(Maps.uniqueIndex(all(), TO_KEY));
@@ -85,7 +126,7 @@ public enum Annotation {
     }
 
     private static final ImmutableSet<Annotation> defaultAnnotations = ImmutableSet.of(
-        ID_SUMMARY
+        ID_SUMMARY, LICENSE
     );
     
 }
