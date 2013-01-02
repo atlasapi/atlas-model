88bda157901bd5c6ed0445d3f4599ae0e81f5504
Oli Hall <oli@metabradcast.com>
2013-01-02 15:19:55 +0000

    Remove ChannelGroupType from ChannelGroup

--- src/main/java/org/atlasapi/media/channel/ChannelGroup.java
+++ src/main/java/org/atlasapi/media/channel/ChannelGroup.java
@@ -13,7 +13,6 @@ public class ChannelGroup extends Identified {
     private Publisher publisher;
     private String title;
     private Set<Country> availableCountries;
-    private ChannelGroupType type;
     private Set<Long> channels = ImmutableSet.of();
 
     public Publisher getPublisher() {
@@ -54,37 +53,5 @@ public class ChannelGroup extends Identified {
     
     public void addChannel(Long id) {
         this.channels = ImmutableSet.<Long>builder().addAll(channels).add(id).build();
-    }
-    
-    public ChannelGroupType getType() {
-        return type;
-    }
-
-    public void setType(ChannelGroupType type) {
-        this.type = type;
-    }
-
-    public enum ChannelGroupType {
-        PLATFORM("platform"),
-        REGION("region");
-        
-        private final String key;
-
-        private ChannelGroupType(String key) {
-            this.key = key;
-        }
-        
-        public String key() {
-            return key;
-        }
-        
-        public static ChannelGroupType fromKey(String key) {
-            for (ChannelGroupType value : ChannelGroupType.values()) {
-                if (value.key.equals(key)) {
-                    return value;
-                }
-            }
-            throw new IllegalArgumentException("Key " + key + " does not map to a ChannelGroupType");
-        }
     }
 }
