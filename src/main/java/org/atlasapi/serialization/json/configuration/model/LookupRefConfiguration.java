10b0171222de3990737857a95cf55686f19f5416
Fred van den Driessche <fred@metabroadcast.com>
2013-01-27 16:35:55 +0000

    add EntityType to ParentRef

--- src/main/java/org/atlasapi/serialization/json/configuration/model/LookupRefConfiguration.java
+++ src/main/java/org/atlasapi/serialization/json/configuration/model/LookupRefConfiguration.java
@@ -1,15 +1,17 @@
 package org.atlasapi.serialization.json.configuration.model;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import org.atlasapi.media.common.Id;
 import org.atlasapi.media.entity.Publisher;
 import org.atlasapi.persistence.content.ContentCategory;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  */
 public abstract class LookupRefConfiguration {
 
     @JsonCreator
-    LookupRefConfiguration(@JsonProperty("id") String id, @JsonProperty("publisher") Publisher publisher, @JsonProperty("category") ContentCategory category) {
+    LookupRefConfiguration(@JsonProperty("id") Id id, @JsonProperty("publisher") Publisher publisher, @JsonProperty("category") ContentCategory category) {
     }
 }
