d24ce5aca5b12596c64b721e0ead317b3b98ce8a
Fred van den Driessche <fred@metabroadcast.com>
2013-01-30 11:02:07 +0000

    Add TopicStore to PersistenceModule interface

--- src/main/java/org/atlasapi/media/PersisitenceModule.java
+++ src/main/java/org/atlasapi/media/PersisitenceModule.java
@@ -1,10 +1,12 @@
 package org.atlasapi.media;
 
 import org.atlasapi.media.content.ContentStore;
-
+import org.atlasapi.media.topic.TopicStore;
 
 public interface PersisitenceModule {
 
     ContentStore contentStore();
+    
+    TopicStore topicStore();
     
 }
