c42ff17c0b4cbbe945f0c78e5999bde6511e348d
Fred van den Driessche <fred@metabroadcast.com>
2013-02-22 10:22:03 +0000

    import Channel{Resolver,Lister} interfaces from atlas-persistence

---
+++ src/main/java/org/atlasapi/media/channel/ChannelLister.java
@@ -0,0 +1,9 @@
+package org.atlasapi.media.channel;
+
+
+/**
+ */
+public interface ChannelLister {
+    
+    Iterable<Channel> all();
+}
---
+++ src/main/java/org/atlasapi/media/channel/ChannelResolver.java
@@ -0,0 +1,28 @@
+package org.atlasapi.media.channel;
+
+import java.util.Map;
+
+import org.atlasapi.media.common.Id;
+
+import com.metabroadcast.common.base.Maybe;
+
+public interface ChannelResolver extends ChannelLister {
+
+	@Deprecated
+	Maybe<Channel> fromKey(String key);
+
+	Maybe<Channel> fromId(Id id);
+	
+	Maybe<Channel> fromUri(String uri);
+	
+	Iterable<Channel> forIds(Iterable<Id> ids);
+		
+	/**
+	 * Return a map whose keys are aliases prefixed with aliasPrefix
+	 * 
+	 * @param aliasPrefix
+	 * @return
+	 */
+	Map<String, Channel> forAliases(String aliasPrefix);
+	
+}
