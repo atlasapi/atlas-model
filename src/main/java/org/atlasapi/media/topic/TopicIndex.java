a880c706280ce366d8bc4ca03d8e237666d25a47
Fred van den Driessche <fred@metabroadcast.com>
2013-03-27 16:43:57 +0000

    TopicIndex.index() throws IndexException

--- src/main/java/org/atlasapi/media/topic/TopicIndex.java
+++ src/main/java/org/atlasapi/media/topic/TopicIndex.java
@@ -2,6 +2,7 @@ package org.atlasapi.media.topic;
 
 import org.atlasapi.content.criteria.AttributeQuerySet;
 import org.atlasapi.media.common.Id;
+import org.atlasapi.media.content.IndexException;
 import org.atlasapi.media.entity.Publisher;
 
 import com.google.common.collect.FluentIterable;
@@ -10,7 +11,7 @@ import com.metabroadcast.common.query.Selection;
 
 public interface TopicIndex {
 
-    void index(Topic topic);
+    void index(Topic topic) throws IndexException;
     
     ListenableFuture<FluentIterable<Id>> query(AttributeQuerySet query, 
         Iterable<Publisher> publishers, Selection selection);
