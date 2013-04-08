7924180395272b7f55b0ba030ee15a11817e9d98
Fred van den Driessche <fred@metabroadcast.com>
2013-04-08 23:58:18 +0100

    add ContentGroup interfaces

---
+++ src/main/java/org/atlasapi/persistence/content/ContentGroupLister.java
@@ -0,0 +1,9 @@
+package org.atlasapi.persistence.content;
+
+import org.atlasapi.media.entity.ContentGroup;
+
+public interface ContentGroupLister {
+    
+    Iterable<ContentGroup> findAll();
+    
+}
