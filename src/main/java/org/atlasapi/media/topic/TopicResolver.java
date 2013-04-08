645fb09131719b9e8b91462879e277bbe8ef07d6
Fred van den Driessche <fred@metabroadcast.com>
2013-04-08 23:20:47 +0100

    use Alias type with resolveAliases for Topic

--- src/main/java/org/atlasapi/media/topic/TopicResolver.java
+++ src/main/java/org/atlasapi/media/topic/TopicResolver.java
@@ -1,12 +1,13 @@
 package org.atlasapi.media.topic;
 
 import org.atlasapi.media.common.IdResolver;
+import org.atlasapi.media.entity.Alias;
 import org.atlasapi.media.entity.Publisher;
 
 import com.metabroadcast.common.collect.OptionalMap;
 
 public interface TopicResolver extends IdResolver<Topic> {
 
-    OptionalMap<String, Topic> resolveAliases(Iterable<String> aliases, Publisher source);
+    OptionalMap<Alias, Topic> resolveAliases(Iterable<Alias> aliases, Publisher source);
     
 }
