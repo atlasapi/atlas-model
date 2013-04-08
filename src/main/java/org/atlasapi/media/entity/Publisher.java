580de5def3acfc6b14c06c4014d823ef915bc599
liamgh <liam@greenhughes.com>
2013-04-08 18:47:47 +0100

    Add Swedish Radio to publishers

--- src/main/java/org/atlasapi/media/entity/Publisher.java
+++ src/main/java/org/atlasapi/media/entity/Publisher.java
@@ -59,7 +59,8 @@ public enum Publisher {
     NETFLIX("Netflix", "gb.netflix.com", Countries.GB, SourceStatus.UNAVAILABLE),
     YOUVIEW("YouView", "youview.com", Countries.GB, SourceStatus.UNAVAILABLE),
     FACEBOOK("Facebook Graph API", "graph.facebook.com", Countries.ALL, SourceStatus.AVAILABLE_ENABLED),
-    SCRAPERWIKI("ScraperWiki", "scraperwiki.com", Countries.ALL, SourceStatus.AVAILABLE_DISABLED);
+    SCRAPERWIKI("ScraperWiki", "scraperwiki.com", Countries.ALL, SourceStatus.AVAILABLE_DISABLED),
+    SVERIGES_RADIO("Sveriges Radio", "sverigesradio.se", Countries.ALL, SourceStatus.AVAILABLE_DISABLED);
 
     private static final Splitter CSV_SPLITTER = Splitter.on(',').trimResults();
 
