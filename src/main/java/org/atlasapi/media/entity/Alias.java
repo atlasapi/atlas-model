bdd398043187709d0f443d292f43aeb70e54aadb
Oli Hall <oli@metabradcast.com>
2013-04-26 15:45:55 +0100

    Add static "uri" string for uri namespace to Alias class.

--- src/main/java/org/atlasapi/media/entity/Alias.java
+++ src/main/java/org/atlasapi/media/entity/Alias.java
@@ -5,6 +5,8 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import com.google.common.base.Objects;
 
 public class Alias {
+    
+    public static final String URI_NAMESPACE = "uri";
 
     private final String namespace;
     private final String value;
