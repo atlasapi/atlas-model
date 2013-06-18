fea6cc36fb4a55013442edd680311519cc52cf27
Oli Hall <oli@metabradcast.com>
2013-06-18 16:54:11 +0100

    Alter theme value to be lower-case with underscores.

--- src/main/java/org/atlasapi/media/entity/ImageTheme.java
+++ src/main/java/org/atlasapi/media/entity/ImageTheme.java
@@ -2,10 +2,10 @@ package org.atlasapi.media.entity;
 
 public enum ImageTheme {
     
-    DARK_OPAQUE("dark opaque"), 
-    LIGHT_OPAQUE("light opaque"), 
-    DARK_TRANSPARENT("dark transparent"),
-    LIGHT_TRANSPARENT("dark transparent");
+    DARK_OPAQUE("dark_opaque"), 
+    LIGHT_OPAQUE("light_opaque"), 
+    DARK_TRANSPARENT("dark_transparent"),
+    LIGHT_TRANSPARENT("dark_transparent");
     
     private final String name;
     
