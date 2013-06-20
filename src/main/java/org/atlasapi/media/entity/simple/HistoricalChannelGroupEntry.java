ba59755e9ec3ff31ff6df4cbd941e700660ea261
Oli Hall <oli@metabradcast.com>
2013-06-20 16:34:48 +0100

    Create history entries with their start date (unfortunately it can't be a private final parameter, thanks to JAXB), so that no entries have null start date.

--- src/main/java/org/atlasapi/media/entity/simple/HistoricalChannelGroupEntry.java
+++ src/main/java/org/atlasapi/media/entity/simple/HistoricalChannelGroupEntry.java
@@ -1,5 +1,7 @@
 package org.atlasapi.media.entity.simple;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import java.util.Date;
 
 import javax.xml.bind.annotation.XmlElement;
@@ -13,18 +15,21 @@ public class HistoricalChannelGroupEntry implements Comparable<HistoricalChannel
     
     private Date startDate;
     private String title;
+    
+    /**
+     * JAXB requires no-args constructor
+     */
+    public HistoricalChannelGroupEntry() { }
+    
+    public HistoricalChannelGroupEntry(LocalDate startDate) {
+        this.startDate = checkNotNull(startDate).toDateTimeAtStartOfDay(DateTimeZone.UTC).toDate();
+    }
 
     @XmlElement(name = "startDate")
     public Date getStartDate() {
         return startDate;
     }
     
-    public void setStartDate(LocalDate startDate) {
-        if (startDate != null) {
-            this.startDate = startDate.toDateTimeAtStartOfDay(DateTimeZone.UTC).toDate();
-        }
-    }
-
     public String getTitle() {
         return title;
     }
