5e3ff30a7bfa8cb48b674c715119bb38a8ab7e6f
Tom McAdam <tom@metabroadcast.com>
2013-10-02 13:43:19 +0100

    Return types of items from search results

--- src/main/java/org/atlasapi/search/model/SearchResults.java
+++ src/main/java/org/atlasapi/search/model/SearchResults.java
@@ -2,19 +2,40 @@ package org.atlasapi.search.model;
 
 import java.util.List;
 
+import org.atlasapi.media.entity.simple.ContentIdentifier;
+
+import com.google.common.base.Function;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
 
 public class SearchResults {
 
 	private List<String> results;
-
+	private List<ContentIdentifier> contentIdentifiers;
+	
 	public SearchResults() { /* for GSON */ }
 	
-	public SearchResults(Iterable<String> uris) {
-		this.results = ImmutableList.copyOf(uris);
+	public SearchResults(Iterable<ContentIdentifier> contentIdentifiers) {
+		this.results = ImmutableList.copyOf(Iterables.transform(contentIdentifiers, TO_URI));
+		this.contentIdentifiers = ImmutableList.copyOf(contentIdentifiers);
 	}
 
+	@Deprecated
 	public List<String> toUris() {
 		return results;
 	}
+	
+	public List<ContentIdentifier> contentIdentifiers() {
+	    return contentIdentifiers;
+	}
+	
+	private static Function<ContentIdentifier, String> TO_URI = new Function<ContentIdentifier, String>() {
+
+        @Override
+        public String apply(ContentIdentifier input) {
+            return input.getUri();
+        }
+	    
+	    
+	};
 }
