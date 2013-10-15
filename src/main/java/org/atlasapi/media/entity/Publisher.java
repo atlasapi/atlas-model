80f47ec0759d7df8fe66a81f0311c6e7a29e65a5
Tom McAdam <tom@metabroadcast.com>
2013-10-15 09:09:31 +0100

    Add new publisher for MetaBroadcast Picks

--- src/main/java/org/atlasapi/media/entity/Publisher.java
+++ src/main/java/org/atlasapi/media/entity/Publisher.java
@@ -69,6 +69,7 @@ public enum Publisher {
     YURI("yuri", "yuri.metabroadcast.com", Countries.ALL, SourceStatus.UNAVAILABLE),
     COYOTE("coyote", "coyote.metabroadcast.com", Countries.ALL, SourceStatus.UNAVAILABLE), 
     BBC_NITRO("BBC Nitro", "nitro.bbc.co.uk", Countries.ALL, SourceStatus.AVAILABLE_DISABLED),
+    METABROADCAST_PICKS("MetaBroadcast Picks", "picks.metabroadcast.com", Countries.GB, SourceStatus.UNAVAILABLE),
     ;
 
     private static final Splitter CSV_SPLITTER = Splitter.on(',').trimResults();
