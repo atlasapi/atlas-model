87b1d860f451b3c016a6bdc69292a6bb791e3304
liamgh <liam@greenhughes.com>
2013-10-25 15:47:23 +0100

    Removing atlas-applications project

---
+++ src/main/java/org/atlasapi/application/users/NewUserSupplier.java
@@ -0,0 +1,22 @@
+package org.atlasapi.application.users;
+
+import com.google.common.base.Supplier;
+import com.metabroadcast.common.ids.IdGenerator;
+import com.metabroadcast.common.ids.SubstitutionTableNumberCodec;
+
+public class NewUserSupplier implements Supplier<User> {
+
+    private final IdGenerator idGenerator;
+    private SubstitutionTableNumberCodec codec;
+
+    public NewUserSupplier(IdGenerator idGenerator) {
+        this.idGenerator = idGenerator;
+        this.codec = new SubstitutionTableNumberCodec();
+    }
+    
+    @Override
+    public User get() {
+        return new User(codec.decode(idGenerator.generate()).longValue());
+    }
+
+}
