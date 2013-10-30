bb42b4f184d6d024c1d37bc023a4dbecc48b36aa
liamgh <liam@greenhughes.com>
2013-10-30 16:38:29 +0000

    Add methods to look up user by id

--- src/main/java/org/atlasapi/application/users/UserStore.java
+++ src/main/java/org/atlasapi/application/users/UserStore.java
@@ -1,5 +1,7 @@
 package org.atlasapi.application.users;
 
+import org.atlasapi.media.common.Id;
+
 import com.google.common.base.Optional;
 import com.metabroadcast.common.social.model.UserRef;
 
@@ -7,7 +9,11 @@ public interface UserStore {
 
     Optional<User> userForRef(UserRef ref);
     
-    Optional<User> userForId(Long userId);
+    Optional<User> userForId(Id id);
+    
+    Iterable<User> usersFor(Iterable<Id> ids);
+    
+    Iterable<User> allUsers();
     
     void store(User user);
     
