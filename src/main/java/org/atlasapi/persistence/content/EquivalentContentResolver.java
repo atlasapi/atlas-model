cf2a4a82b47f1be47f50bfa93fcc225c697ad05d
Fred van den Driessche <fred@metabroadcast.com>
2013-11-01 18:26:09 +0000

    add resolveAliases method to EquivalentContentResolver

--- src/main/java/org/atlasapi/persistence/content/EquivalentContentResolver.java
+++ src/main/java/org/atlasapi/persistence/content/EquivalentContentResolver.java
@@ -3,8 +3,9 @@ package org.atlasapi.persistence.content;
 import java.util.Set;
 
 import org.atlasapi.application.ApplicationConfiguration;
-import org.atlasapi.media.entity.Publisher;
 import org.atlasapi.output.Annotation;
+
+import com.google.common.base.Optional;
 
 /**
  * A content resolver which returns the equivalent set for each requested
@@ -15,7 +16,7 @@ public interface EquivalentContentResolver {
 
     /**
      * Resolves the equivalent sets of content for a given set of source URIs.
-     * Only content from the given sources is resolved.
+     * Only content from enabled sources in the app config is resolved.
      * 
      * @param uris
      *            - requested URI keys of equivalent content.
@@ -31,7 +32,7 @@ public interface EquivalentContentResolver {
     
     /**
      * Resolves the equivalent sets of content for a given set of source URIs.
-     * Only content from the given sources is resolved.
+     * Only content from enabled sources in the app config is resolved.
      * 
      * @param ids
      *            - requested numeric keys of equivalent content.
@@ -42,5 +43,19 @@ public interface EquivalentContentResolver {
      * @return EquivalentContent with an entry for each of the requested URIs.
      */
     EquivalentContent resolveIds(Iterable<Long> ids, ApplicationConfiguration appConfig, Set<Annotation> activeAnnotations);
+    
+    /**
+     * Resolves the equivalent sets of content for a possible namespace and
+     * alias values. Only content from enabled sources in the app config is resolved.
+     * 
+     * @param ids
+     *            - requested numeric keys of equivalent content.
+     * @param appConfig
+     *            - configuration used to filter content.
+     * @param activeAnnotations
+     *            - components of the model to resolve.
+     * @return EquivalentContent with an entry for each of the requested URIs.
+     */
+    EquivalentContent resolveAliases(Optional<String> namespace, Iterable<String> values, ApplicationConfiguration appConfig, Set<Annotation> activeAnnotations);
 
 }
