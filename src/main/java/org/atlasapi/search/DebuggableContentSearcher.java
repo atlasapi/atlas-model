6e5de330010b6e84ea6aa543516047c5cab5aaff
Tom McAdam <tom@metabroadcast.com>
2014-01-10 15:20:36 +0000

    Add debug method to get indexed document

--- src/main/java/org/atlasapi/search/DebuggableContentSearcher.java
+++ src/main/java/org/atlasapi/search/DebuggableContentSearcher.java
@@ -2,7 +2,11 @@ package org.atlasapi.search;
 
 import org.atlasapi.search.model.SearchQuery;
 
+import com.google.common.base.Optional;
+
 public interface DebuggableContentSearcher extends ContentSearcher {
     
     String debug(SearchQuery query);
+
+    Optional<String> document(String key);
 }
