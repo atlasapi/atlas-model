c49db4ea269f8524126d6899101c9feea5ee2967
Tom McAdam <tom@metabroadcast.com>
2014-01-28 10:54:31 +0000

    Fix formatting on simple Aliased - no functional change

--- src/main/java/org/atlasapi/media/entity/simple/Aliased.java
+++ src/main/java/org/atlasapi/media/entity/simple/Aliased.java
@@ -12,41 +12,41 @@ import com.google.common.collect.Sets;
 
 public abstract class Aliased extends Identified {
 
-	protected Set<String> aliases = Sets.newHashSet();
-	protected Set<Alias> v4Aliases = Sets.newHashSet();
-	
-	public Aliased(String uri) {
-		super(uri);
-	}
-	
-	public Aliased() { /* required for XML/JSON tools */	}
-	
-	public void setAliases(Set<String> aliases) {
-		this.aliases = aliases;
-	}
-	
-	public void setV4Aliases(Set<Alias> v4Aliases) {
-	    this.v4Aliases = v4Aliases;
-	}
-	
-	@XmlElementWrapper(name="aliases")
-	@XmlElement(name="alias")
-	public Set<String> getAliases() {
-		return aliases;
-	}
-	
-	@XmlElementWrapper(name="v4aliases")
+    protected Set<String> aliases = Sets.newHashSet();
+    protected Set<Alias> v4Aliases = Sets.newHashSet();
+
+    public Aliased(String uri) {
+        super(uri);
+    }
+
+    public Aliased() { /* required for XML/JSON tools */	}
+
+    public void setAliases(Set<String> aliases) {
+        this.aliases = aliases;
+    }
+
+    public void setV4Aliases(Set<Alias> v4Aliases) {
+        this.v4Aliases = v4Aliases;
+    }
+
+    @XmlElementWrapper(name="aliases")
+    @XmlElement(name="alias")
+    public Set<String> getAliases() {
+        return aliases;
+    }
+
+    @XmlElementWrapper(name="v4aliases")
     @XmlElement(name="alias")
     public Set<Alias> getV4Aliases() {
         return v4Aliases;
     }
-	
-	protected void copyTo(Aliased destination) {
+
+    protected void copyTo(Aliased destination) {
         Preconditions.checkNotNull(destination);
-        
+
         super.copyTo(destination);
-        
+
         destination.setAliases(getAliases());
     }
-	
+
 }
