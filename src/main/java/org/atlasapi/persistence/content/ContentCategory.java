dc95ca0fd0a599ea26a072099972c780d46ca655
Massimiliano Tomassi <max@metabroadcast.com>
2014-10-23 15:17:10 +0100

    Added a TO_NAME function to ContentCategory

--- src/main/java/org/atlasapi/persistence/content/ContentCategory.java
+++ src/main/java/org/atlasapi/persistence/content/ContentCategory.java
@@ -1,13 +1,13 @@
 package org.atlasapi.persistence.content;
 
-
 import org.atlasapi.media.entity.Container;
+import org.atlasapi.media.entity.ContentGroup;
 import org.atlasapi.media.entity.Described;
 import org.atlasapi.media.entity.Item;
 import org.atlasapi.media.entity.Series;
 
+import com.google.common.base.Function;
 import com.google.common.collect.ImmutableSet;
-import org.atlasapi.media.entity.ContentGroup;
 
 public enum ContentCategory {
 
@@ -22,6 +22,13 @@ public enum ContentCategory {
     public static final ImmutableSet<ContentCategory> ITEMS = ImmutableSet.of(TOP_LEVEL_ITEM, CHILD_ITEM);
     public static final ImmutableSet<ContentCategory> CONTAINERS = ImmutableSet.of(CONTAINER, PROGRAMME_GROUP);
     public static final ImmutableSet<ContentCategory> TOP_LEVEL_CONTENT = ImmutableSet.of(CONTENT_GROUP, CONTAINER, TOP_LEVEL_ITEM);
+
+    public static final Function<ContentCategory, String> TO_NAME = new Function<ContentCategory, String>() {
+        @Override
+        public String apply(ContentCategory input) {
+            return input.name();
+        }
+    };
 
     private String name;
 
