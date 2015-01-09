2b7a8fc398f9225aa7cd91dae1049af558735599
Tom McAdam <tom@metabroadcast.com>
2015-01-09 10:31:30 +0000

    Add alias to simple Version

--- src/main/java/org/atlasapi/media/entity/simple/Version.java
+++ src/main/java/org/atlasapi/media/entity/simple/Version.java
@@ -1,8 +1,12 @@
 package org.atlasapi.media.entity.simple;
 
+import java.util.Set;
+
 import javax.xml.bind.annotation.XmlElement;
 
 import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
 
 
 public abstract class Version {
@@ -11,6 +15,15 @@ public abstract class Version {
 	private Integer publishedDuration;
 	private Restriction restriction;
 	private Boolean is3d;
+	private Set<Alias> aliases = Sets.newHashSet();
+	
+	public Set<Alias> getAliases() {
+	    return aliases;
+	}
+	
+	public void setAliases(Iterable<Alias> aliases) {
+	    this.aliases = ImmutableSet.copyOf(aliases);
+	}
 
 	public Integer getDuration() {
 		return duration;
@@ -54,5 +67,6 @@ public abstract class Version {
 	        destination.setRestriction(getRestriction().copy());
 	    }
 	    destination.set3d(is3d());
+	    destination.setAliases(getAliases());
 	}
 }
