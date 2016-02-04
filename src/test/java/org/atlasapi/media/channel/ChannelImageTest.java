6e0187eecd0d931a4ad709d159c46b9ae8a9f156
Timothy Sum Hon Mun <timothy@metabroadcast.com>
2016-02-04 18:32:38 +0000

    Remove old generic type from ChannelImageTest

--- src/test/java/org/atlasapi/media/channel/ChannelImageTest.java
+++ src/test/java/org/atlasapi/media/channel/ChannelImageTest.java
@@ -68,7 +68,6 @@ public class ChannelImageTest {
         TemporalField<Image> futureNormal = new TemporalField<Image>(createNormalImage("future normal"), now.plusYears(2), now.plusYears(3));
         TemporalField<Image> currentDt = new TemporalField<Image>(createDarkTransparentImage("current dt"), now.minusYears(2), now.plusYears(2));
         TemporalField<Image> currentLt = new TemporalField<Image>(createLightTransparentImage("current lt"), now.minusYears(2), now.plusYears(2));
-        TemporalField<Image> currentLtGeneric = new TemporalField<Image>(createImageWithGenericType("current ltgeneric"), now.minusYears(2), now.plusYears(2));
 
         Channel channel = Channel.builder()
                 .build();
@@ -109,13 +108,4 @@ public class ChannelImageTest {
         return image;
     }
 
-    private Image createImageWithGenericType(String uri) {
-        Image image = new Image(uri);
-
-        image.setTheme(ImageTheme.LIGHT_TRANSPARENT);
-        image.setType(ImageType.GENERIC);
-        image.setColor(ImageColor.COLOR);
-
-        return image;
-    }
 }
