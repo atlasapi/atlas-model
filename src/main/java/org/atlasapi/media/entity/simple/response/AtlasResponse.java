000f8bfd91c63e24bdc5a9941efbde88c8ae66d6
Lavanya Jeyaratnam <lavanya@metabroadcast.com>
2016-07-26 20:18:31 +0100

    created a new class clalled AtlasResponse - which can be used to return id when posting to atlas

---
+++ src/main/java/org/atlasapi/media/entity/simple/response/AtlasResponse.java
@@ -0,0 +1,15 @@
+package org.atlasapi.media.entity.simple.response;
+
+
+public class AtlasResponse {
+
+    private final String id;
+
+    public AtlasResponse(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
