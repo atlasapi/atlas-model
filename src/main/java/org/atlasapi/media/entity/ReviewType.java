112aa94929496184d7368a1c8ccac3ae1e0b4cf4
Jamie Coleman <jamiec@metabroadcast.com>
2017-03-09 14:55:25 +0000

    add source to reviews

--- src/main/java/org/atlasapi/media/entity/ReviewType.java
+++ src/main/java/org/atlasapi/media/entity/ReviewType.java
@@ -1,5 +1,7 @@
 package org.atlasapi.media.entity;
 
+import com.google.common.base.Strings;
+
 import javax.annotation.Nullable;
 import java.util.Arrays;
 
@@ -25,7 +27,11 @@ public enum ReviewType {
     }
 
     @Nullable
-    public static ReviewType fromKey(String key) {
+    public static ReviewType fromKey(@Nullable String key) {
+
+        if (Strings.isNullOrEmpty(key)) {
+            return null;
+        }
 
         return Arrays.stream(ReviewType.values())
                 .filter(reviewType -> reviewType.name().equalsIgnoreCase(key))
