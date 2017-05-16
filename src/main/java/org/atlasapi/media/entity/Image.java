2c81e56538afd9f256d246308522c78c9427a88a
dragons <piticadragos@yahoo.com>
2017-05-16 11:08:17 +0100

    fixed sonar issues

--- src/main/java/org/atlasapi/media/entity/Image.java
+++ src/main/java/org/atlasapi/media/entity/Image.java
@@ -1,13 +1,32 @@
 package org.atlasapi.media.entity;
 
-import org.joda.time.DateTime;
-
 import com.google.common.base.Predicate;
 import com.metabroadcast.common.media.MimeType;
+import org.joda.time.DateTime;
 
 public class Image extends Identified {
-    
-    public static final Predicate<Image> IS_PRIMARY = input -> input.getType() != null && input.getType().equals(ImageType.PRIMARY);
+
+    public static final Predicate<Image> IS_PRIMARY = input -> input.getType() != null
+            && input.getType().equals(ImageType.PRIMARY);
+    public static final Predicate<Image> IS_AVAILABLE = input -> (
+            input.getAvailabilityStart() == null || !(new DateTime(input.getAvailabilityStart()).isAfterNow())
+    )
+            && (input.getAvailabilityEnd() == null || new DateTime(input.getAvailabilityEnd()).isAfterNow());
+
+    private ImageType type;
+    private ImageColor color;
+    private ImageTheme theme;
+    private Integer height;
+    private Integer width;
+    private ImageAspectRatio aspectRatio;
+    private MimeType mimeType;
+    private DateTime availabilityStart;
+    private DateTime availabilityEnd;
+    private Boolean hasTitleArt;
+
+    public Image(String uri) {
+        super(uri);
+    }
 
     public static final Builder builder(Image base) {
         Builder builder = new Builder(base.getCanonicalUri());
@@ -23,135 +42,31 @@ public class Image extends Identified {
         builder.withHasTitleArt(base.hasTitleArt);
         return builder;
     }
-    
+
     public static final Builder builder(String uri) {
         return new Builder(uri);
     }
-    
-    public static final class Builder {
 
-        private String uri;
-        private Integer height;
-        private Integer width;
-        private ImageType type;
-        private ImageColor color;
-        private ImageTheme theme;
-        private ImageAspectRatio aspectRatio;
-        private MimeType mimeType;
-        private DateTime availabilityStart;
-        private DateTime availabilityEnd;
-        private Boolean hasTitleArt;
-
-        public Builder(String uri) {
-            this.uri = uri;
-        }
-        
-        public Builder withUri(String uri) {
-            this.uri = uri;
-            return this;
-        }
-        
-        public Builder withHeight(Integer height) {
-            this.height = height;
-            return this;
-        }
-        
-        public Builder withWidth(Integer width) {
-            this.width = width;
-            return this;
-        }
-        
-        public Builder withType(ImageType type) {
-            this.type = type;
-            return this;
-        }
-        
-        public Builder withColor(ImageColor color) {
-            this.color = color;
-            return this;
-        }
-        
-        public Builder withTheme(ImageTheme theme) {
-            this.theme = theme;
-            return this;
-        }
-        
-        public Builder withAspectRatio(ImageAspectRatio aspectRatio) {
-            this.aspectRatio = aspectRatio;
-            return this;
-        }
-        
-        public Builder withMimeType(MimeType mimeType) {
-            this.mimeType = mimeType;
-            return this;
-        }
-        
-        public Builder withAvailabilityStart(DateTime availabilityStart) {
-            this.availabilityStart = availabilityStart;
-            return this;
-        }
-        
-        public Builder withAvailabilityEnd(DateTime availabilityEnd) {
-            this.availabilityEnd = availabilityEnd;
-            return this;
-        }
-
-        public Builder withHasTitleArt(Boolean hasTitleArt) {
-            this.hasTitleArt = hasTitleArt;
-            return this;
-        }
-
-        public Image build() {
-            Image image = new Image(uri);
-            image.setHeight(height);
-            image.setWidth(width);
-            image.setType(type);
-            image.setColor(color);
-            image.setTheme(theme);
-            image.setAspectRatio(aspectRatio);
-            image.setMimeType(mimeType);
-            image.setAvailabilityStart(availabilityStart);
-            image.setAvailabilityEnd(availabilityEnd);
-            image.setHasTitleArt(hasTitleArt);
-            return image;
-        }
-    }
-
-    public Image(String uri) {
-        super(uri);
-    }
-    
-    private ImageType type;
-    private ImageColor color;
-    private ImageTheme theme;
-    private Integer height;
-    private Integer width;
-    private ImageAspectRatio aspectRatio;
-    private MimeType mimeType;
-    private DateTime availabilityStart;
-    private DateTime availabilityEnd;
-    private Boolean hasTitleArt;
-    
     public Integer getHeight() {
         return height;
     }
-    
+
     public void setHeight(Integer height) {
         this.height = height;
     }
-    
+
     public Integer getWidth() {
         return width;
     }
-    
+
     public void setWidth(Integer width) {
         this.width = width;
     }
-    
+
     public ImageType getType() {
         return type;
     }
-    
+
     public void setType(ImageType type) {
         this.type = type;
     }
@@ -187,19 +102,19 @@ public class Image extends Identified {
     public void setMimeType(MimeType mimeType) {
         this.mimeType = mimeType;
     }
-    
+
     public DateTime getAvailabilityStart() {
         return availabilityStart;
     }
-    
+
     public void setAvailabilityStart(DateTime availabilityStart) {
         this.availabilityStart = availabilityStart;
     }
-    
+
     public DateTime getAvailabilityEnd() {
         return availabilityEnd;
     }
-    
+
     public void setAvailabilityEnd(DateTime availabilityEnd) {
         this.availabilityEnd = availabilityEnd;
     }
@@ -212,11 +127,92 @@ public class Image extends Identified {
         this.hasTitleArt = hasTitleArt;
     }
 
-    public static final Predicate<Image> IS_AVAILABLE = new Predicate<Image>() {
-        @Override
-        public boolean apply(Image input) {
-            return (input.getAvailabilityStart() == null || ! (new DateTime(input.getAvailabilityStart()).isAfterNow()))
-                    && (input.getAvailabilityEnd() == null || new DateTime(input.getAvailabilityEnd()).isAfterNow());
+    public static final class Builder {
+
+        private String uri;
+        private Integer height;
+        private Integer width;
+        private ImageType type;
+        private ImageColor color;
+        private ImageTheme theme;
+        private ImageAspectRatio aspectRatio;
+        private MimeType mimeType;
+        private DateTime availabilityStart;
+        private DateTime availabilityEnd;
+        private Boolean hasTitleArt;
+
+        public Builder(String uri) {
+            this.uri = uri;
         }
-    };
+
+        public Builder withUri(String uri) {
+            this.uri = uri;
+            return this;
+        }
+
+        public Builder withHeight(Integer height) {
+            this.height = height;
+            return this;
+        }
+
+        public Builder withWidth(Integer width) {
+            this.width = width;
+            return this;
+        }
+
+        public Builder withType(ImageType type) {
+            this.type = type;
+            return this;
+        }
+
+        public Builder withColor(ImageColor color) {
+            this.color = color;
+            return this;
+        }
+
+        public Builder withTheme(ImageTheme theme) {
+            this.theme = theme;
+            return this;
+        }
+
+        public Builder withAspectRatio(ImageAspectRatio aspectRatio) {
+            this.aspectRatio = aspectRatio;
+            return this;
+        }
+
+        public Builder withMimeType(MimeType mimeType) {
+            this.mimeType = mimeType;
+            return this;
+        }
+
+        public Builder withAvailabilityStart(DateTime availabilityStart) {
+            this.availabilityStart = availabilityStart;
+            return this;
+        }
+
+        public Builder withAvailabilityEnd(DateTime availabilityEnd) {
+            this.availabilityEnd = availabilityEnd;
+            return this;
+        }
+
+        public Builder withHasTitleArt(Boolean hasTitleArt) {
+            this.hasTitleArt = hasTitleArt;
+            return this;
+        }
+
+        public Image build() {
+            Image image = new Image(uri);
+            image.setHeight(height);
+            image.setWidth(width);
+            image.setType(type);
+            image.setColor(color);
+            image.setTheme(theme);
+            image.setAspectRatio(aspectRatio);
+            image.setMimeType(mimeType);
+            image.setAvailabilityStart(availabilityStart);
+            image.setAvailabilityEnd(availabilityEnd);
+            image.setHasTitleArt(hasTitleArt);
+            return image;
+        }
+    }
 }
