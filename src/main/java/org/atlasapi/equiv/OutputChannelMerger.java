056f40ecc4185e4a4ca36391e99c153c08167e0c
jamiesprax <jamiec@metabroadcast.com>
2017-05-30 15:46:03 +0100

    Mbst 18474/implement channel equiv (#345)
    * add support for channel equiv

---
+++ src/main/java/org/atlasapi/equiv/OutputChannelMerger.java
@@ -0,0 +1,94 @@
+package org.atlasapi.equiv;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Ordering;
+import com.metabroadcast.applications.client.model.internal.Application;
+import com.metabroadcast.common.stream.MoreCollectors;
+import org.atlasapi.media.channel.Channel;
+import org.atlasapi.media.entity.Publisher;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.StreamSupport;
+
+public class OutputChannelMerger {
+
+    private OutputChannelMerger() {}
+
+    public static OutputChannelMerger create() {
+        return new OutputChannelMerger();
+    }
+
+    public Channel merge(Application application, Channel channel, Iterable<Channel> equivalents) {
+
+        if (!application.getConfiguration().isPrecedenceEnabled()) {
+            return channel;
+        }
+
+        Map<Publisher, Channel> channelMap = Maps.newHashMap();
+
+        channelMap.put(channel.getSource(), channel);
+
+        StreamSupport.stream(equivalents.spliterator(), false)
+                .filter(equivalentChannel -> application.getConfiguration()
+                        .getEnabledReadSources()
+                        .contains(equivalentChannel.getSource())
+                )
+                .forEach(equivalentChannel ->
+                        channelMap.put(equivalentChannel.getSource(), equivalentChannel)
+                );
+
+        Channel mergedChannel = Channel.builder(channel).build();
+        mergedChannel.setId(channel.getId());
+
+        ImmutableList<Publisher> orderedPublishers = getOrderedPublishers(application);
+
+        mergeAdvertiseFrom(orderedPublishers, channelMap, mergedChannel);
+        mergeAdvertiseTo(orderedPublishers, channelMap, mergedChannel);
+
+        return mergedChannel;
+    }
+
+    private ImmutableList<Publisher> getOrderedPublishers(
+            com.metabroadcast.applications.client.model.internal.Application application) {
+        Ordering<Publisher> ordering = application.getConfiguration()
+                .getImageReadPrecedenceOrdering();
+
+        return application.getConfiguration()
+                .getEnabledReadSources()
+                .stream()
+                .sorted(ordering)
+                .collect(MoreCollectors.toImmutableList());
+
+    }
+
+    private void mergeAdvertiseFrom(
+            List<Publisher> publishers,
+            Map<Publisher, Channel> channelMap,
+            Channel mergedChannel
+    ) {
+        for (Publisher publisher : publishers) {
+            Channel channel = channelMap.get(publisher);
+            if(channel != null && channel.getAdvertiseFrom() != null) {
+                mergedChannel.setAdvertiseFrom(channel.getAdvertiseFrom());
+                break;
+            }
+        }
+    }
+
+    private void mergeAdvertiseTo(
+            List<Publisher> publishers,
+            Map<Publisher, Channel> channelMap,
+            Channel mergedChannel
+    ) {
+        for (Publisher publisher : publishers) {
+            Channel channel = channelMap.get(publisher);
+            if(channel != null && channel.getAdvertiseTo() != null) {
+                mergedChannel.setAdvertiseTo(channel.getAdvertiseTo());
+                break;
+            }
+        }
+    }
+
+}
---
+++ src/test/java/org/atlasapi/equiv/OutputChannelMergerTest.java
@@ -0,0 +1,204 @@
+package org.atlasapi.equiv;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.metabroadcast.applications.client.model.internal.Application;
+import com.metabroadcast.applications.client.model.internal.ApplicationConfiguration;
+import com.metabroadcast.common.stream.MoreCollectors;
+import org.atlasapi.media.channel.Channel;
+import org.atlasapi.media.entity.Alias;
+import org.atlasapi.media.entity.Publisher;
+import org.hamcrest.Matchers;
+import org.jmock.auto.Mock;
+import org.joda.time.DateTime;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.annotation.Nullable;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class OutputChannelMergerTest {
+
+    private OutputChannelMerger channelMerger;
+    private Map<Long, Channel> testChannelStore;
+
+    @Mock private Application btProdApp = mock(Application.class);
+    @Mock private Application btTest1App = mock(Application.class);
+    @Mock private Application noPrecedenceApp = mock(Application.class);
+
+
+    @Before
+    public void setUp() {
+        this.channelMerger = OutputChannelMerger.create();
+        buildTestChannels();
+
+        when(btProdApp.getConfiguration()).thenReturn(
+                ApplicationConfiguration.builder()
+                    .withPrecedence(ImmutableList.of(
+                            Publisher.BT_TVE_VOD,
+                            Publisher.BT_TV_CHANNELS,
+                            Publisher.BT_TV_CHANNELS_TEST1,
+                            Publisher.PA,
+                            Publisher.METABROADCAST
+                    ))
+                    .withEnabledWriteSources(ImmutableList.of())
+                    .build()
+        );
+
+        when(btTest1App.getConfiguration()).thenReturn(
+                ApplicationConfiguration.builder()
+                    .withPrecedence(ImmutableList.of(
+                            Publisher.BT_TVE_VOD,
+                            Publisher.BT_TV_CHANNELS_TEST1,
+                            Publisher.PA,
+                            Publisher.METABROADCAST
+                    ))
+                    .withEnabledWriteSources(ImmutableList.of())
+                    .build()
+        );
+
+        when(noPrecedenceApp.getConfiguration()).thenReturn(
+                ApplicationConfiguration.builder()
+                        .withNoPrecedence(ImmutableList.of(
+                                Publisher.BT_TVE_VOD,
+                                Publisher.PA,
+                                Publisher.METABROADCAST,
+                                Publisher.BT_TV_CHANNELS_TEST1
+                        ))
+                        .withEnabledWriteSources(ImmutableList.of())
+                        .build()
+        );
+
+    }
+
+    @Test
+    public void channelIsMergedWithPrecedentData() throws Exception {
+
+        Channel requestedChannel = testChannelStore.get(1L);
+        Channel expectedEquivalent = testChannelStore.get(3L);
+        Iterable<Channel> resolvedEquivalents = resolveSameAs(requestedChannel.getSameAs());
+
+        Channel mergedChannel = channelMerger.merge(btProdApp, requestedChannel, resolvedEquivalents);
+
+        checkIdAndSource(mergedChannel, requestedChannel);
+        assertThat(mergedChannel.getAdvertiseFrom(), is(expectedEquivalent.getAdvertiseFrom()));
+        assertThat(mergedChannel.getAdvertiseTo(), is(expectedEquivalent.getAdvertiseTo()));
+    }
+
+    @Test
+    public void channelMergesWithPrecedenceAndUsesExistingAsFallback() throws Exception {
+
+        Channel requestedChannel = testChannelStore.get(1L);
+        Channel expectedEquivalent = testChannelStore.get(4L);
+        Iterable<Channel> resolvedEquivalents = resolveSameAs(requestedChannel.getSameAs());
+
+        Channel mergedChannel = channelMerger.merge(btTest1App, requestedChannel, resolvedEquivalents);
+
+        checkIdAndSource(mergedChannel, requestedChannel);
+        assertThat(mergedChannel.getAdvertiseFrom(), is(expectedEquivalent.getAdvertiseFrom()));
+        assertThat(mergedChannel.getAdvertiseTo(), is(requestedChannel.getAdvertiseTo()));
+
+    }
+
+    @Test
+    public void channelDoesntMergeOnApplicationWithNoPrecedence() throws Exception {
+
+        Channel requestedChannel = testChannelStore.get(1L);
+        Iterable<Channel> resolvedEquivalents = resolveSameAs(requestedChannel.getSameAs());
+
+        Channel mergedChannel = channelMerger.merge(noPrecedenceApp, requestedChannel, resolvedEquivalents);
+
+        checkIdAndSource(mergedChannel, requestedChannel);
+        assertThat(mergedChannel.getAdvertiseFrom(), is(requestedChannel.getAdvertiseFrom()));
+        assertThat(mergedChannel.getAdvertiseTo(), is(requestedChannel.getAdvertiseTo()));
+
+    }
+
+    @Test
+    public void channelAdvertiseDatesAreNullWhenNoPrecedenceOnApplication() throws Exception {
+
+        Channel requestedChannel = testChannelStore.get(2L);
+        Iterable<Channel> resolvedEquivalents = resolveSameAs(requestedChannel.getSameAs());
+
+        Channel mergedChannel = channelMerger.merge(noPrecedenceApp, requestedChannel, resolvedEquivalents);
+
+        assertThat(mergedChannel.getId(), is(requestedChannel.getId()));
+        assertThat(mergedChannel.getAdvertiseFrom(), is(Matchers.nullValue()));
+        assertThat(mergedChannel.getAdvertiseTo(), is(Matchers.nullValue()));
+        assertThat(mergedChannel.getSource(), is(requestedChannel.getSource()));
+    }
+
+    @Test
+    public void channelDoesntMergeWhenRequestingEquivalentChannel() throws Exception {
+
+        Channel requestedChannel = testChannelStore.get(5L);
+        Iterable<Channel> resolvedEquivalents = resolveSameAs(requestedChannel.getSameAs());
+
+        Channel mergedChannel = channelMerger.merge(btProdApp, requestedChannel, resolvedEquivalents);
+
+        checkIdAndSource(mergedChannel, requestedChannel);
+        assertThat(mergedChannel.getAdvertiseFrom(), is(requestedChannel.getAdvertiseFrom()));
+        assertThat(mergedChannel.getAdvertiseTo(), is(requestedChannel.getAdvertiseTo()));
+    }
+
+
+    private void checkIdAndSource(Channel mergedChannel, Channel requestedChannel) {
+        assertThat(mergedChannel.getId(), is(requestedChannel.getId()));
+        assertThat(mergedChannel.getSource(), is(requestedChannel.getSource()));
+    }
+
+    private Iterable<Channel> resolveSameAs(Set<ChannelRef> refs) {
+        return refs.stream()
+                .map(ChannelRef::getId)
+                .map(id -> testChannelStore.get(id))
+                .collect(MoreCollectors.toImmutableList());
+    }
+
+    private void buildTestChannels() {
+
+        DateTime now = DateTime.now()
+                .withTime(0,0,0,0);
+
+        testChannelStore = ImmutableMap.of(
+            1L, createChannel(1L, Publisher.METABROADCAST, now.minusDays(3), now.plusDays(3), 1L, 3L, 4L),
+            2L, createChannel(2L, Publisher.METABROADCAST, null, null, 2L, 5L),
+            3L, createChannel(3L, Publisher.BT_TV_CHANNELS, now.minusDays(1), now.plusDays(1), 3L),
+            4L, createChannel(4L, Publisher.BT_TV_CHANNELS_TEST1, now.minusDays(2), null, 4L),
+            5L, createChannel(5L, Publisher.BT_TV_CHANNELS_TEST1, now.minusDays(7), now.plusDays(7), 5L)
+        );
+    }
+
+    private Channel createChannel(
+            long id,
+            Publisher source,
+            @Nullable DateTime advFrom,
+            @Nullable DateTime advTo,
+            long... equivIds
+    ) {
+        Channel channel = Channel.builder()
+                .withSource(source)
+                .withSameAs(Arrays.stream(equivIds)
+                        .mapToObj(eId -> ChannelRef.create(eId, source.key() + eId, source))
+                        .collect(Collectors.toSet())
+                )
+                .withAliases(
+                        ImmutableSet.of(new Alias("pa:channel:id", "9" + id))
+                )
+                .withAdvertiseFrom(advFrom)
+                .withAdvertiseTo(advTo)
+                .build();
+        channel.setId(id);
+
+        return channel;
+    }
+
+}
