9f24c4166a8d68a168a2885c070caf0d81d871f8
Adam Millican <am886@kent.ac.uk>
2017-06-19 18:30:08 +0100

    adding two new publishers for BARB

--- src/main/java/org/atlasapi/media/entity/Publisher.java
+++ src/main/java/org/atlasapi/media/entity/Publisher.java
@@ -227,6 +227,8 @@ public enum Publisher {
     AMC_EBS("Amc EBS", "ebs.amc.com", Countries.GB, SourceStatus.ENABLEABLE, SourceRestriction.ADMIN_ONLY, false),
     TWITCH("Twitch", "twitch.tv", Countries.ALL, SourceStatus.ENABLEABLE, SourceRestriction.ADMIN_ONLY, false),
     C4_PRESS("Channel 4 Press", "press.channel4.com", Countries.GB, SourceStatus.ENABLEABLE, SourceRestriction.ADMIN_ONLY, false),
+    BARB_MASTER("Barb Master File", "master.barb.co.uk", Countries.GB, SourceStatus.ENABLEABLE, SourceRestriction.ADMIN_ONLY, false),
+    BARB_TRANSMISSIONS("Barb Transmissions", "transmissions.barb.co.uk", Countries.GB, SourceStatus.ENABLEABLE, SourceRestriction.ADMIN_ONLY, false)
     ;
 
     private static final Splitter CSV_SPLITTER = Splitter.on(',').trimResults();
