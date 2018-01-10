73acaa9cce169f83016d5338c6c1af3bbb1eff9d
Tristan Beardmore-Gray <tristan.beardmore-gray@hotmail.co.uk>
2018-01-10 15:05:39 +0000

    Added deserialization to WriteResponse

--- src/main/java/org/atlasapi/media/entity/simple/response/WriteResponse.java
+++ src/main/java/org/atlasapi/media/entity/simple/response/WriteResponse.java
@@ -1,11 +1,17 @@
 package org.atlasapi.media.entity.simple.response;
 
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class WriteResponse {
 
     private final String id;
 
-    public WriteResponse(String id) {
+    @JsonCreator
+    public WriteResponse(
+            @JsonProperty("id") String id
+    ) {
         this.id = id;
     }
 
