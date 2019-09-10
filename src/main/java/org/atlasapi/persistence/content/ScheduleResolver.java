7fb6baabc64e76f2aef11daa9936886e313434a3
Tristan Beardmore-Gray <tristan.beardmore-gray@hotmail.co.uk>
2019-09-10 11:04:26 +0100

    Added a resolveItems method to ScheduleResolver interface

--- src/main/java/org/atlasapi/persistence/content/ScheduleResolver.java
+++ src/main/java/org/atlasapi/persistence/content/ScheduleResolver.java
@@ -1,12 +1,14 @@
 package org.atlasapi.persistence.content;
 
+import com.google.common.base.Optional;
 import com.metabroadcast.applications.client.model.internal.Application;
 import org.atlasapi.media.channel.Channel;
+import org.atlasapi.media.entity.Item;
 import org.atlasapi.media.entity.Publisher;
 import org.atlasapi.media.entity.Schedule;
 import org.joda.time.DateTime;
 
-import com.google.common.base.Optional;
+import java.util.Set;
 
 public interface ScheduleResolver {
 
@@ -15,5 +17,7 @@ public interface ScheduleResolver {
     Schedule unmergedSchedule(DateTime from, DateTime to, Iterable<Channel> channels, Iterable<Publisher> publisher);
     
     Schedule schedule(DateTime from, int count, Iterable<Channel> channels, Iterable<Publisher> publisher, Optional<Application> mergeApplication);
+
+    Set<Item> resolveItems(DateTime from, DateTime to, Iterable<Channel> channels, Iterable<Publisher> publishers);
     
 }
